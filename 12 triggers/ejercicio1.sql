d40a44f295f819830497f335d3c45a3c11d80917
ByronMoreno <Byron.moreno.dlu@hotmail.com>
2025-11-11 20:46:46 -0500

    ejericio 1 trigger mas

--- 12 triggers/ejercicio1.sql
+++ 12 triggers/ejercicio1.sql
@@ -25,6 +25,20 @@ FOR EACH ROW
 EXECUTE FUNCTION ft_audit_update_orders();
 
 select * from orders
-where order_id=11078
+where order_id=11078;
 
-select * from orders_audit_update
+select * from orders_audit_update;
+select * from orders_audit;
+
+CREATE OR REPLACE TRIGGER tg_after_delete_order
+AFTER DELETE on orders
+FOR EACH ROW 
+EXECUTE FUNCTION ft_audit_update_orders();
+
+select * from orders;
+--Insertar un valor, luego lo siguiente
+
+select * from orders
+where order_id=11111;
+
+select * from orders_audit_update;
