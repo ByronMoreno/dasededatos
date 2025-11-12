1c265554c52ef4869a655efba5bbeaf580342049
ByronMoreno <Byron.moreno.dlu@hotmail.com>
2025-11-11 20:32:01 -0500

    ejericio 1 trigger

---
+++ 12 triggers/ejercicio1.sql
@@ -0,0 +1,30 @@
+CREATE TABLE orders_audit_update (
+    audit_id SERIAL PRIMARY KEY,
+    order_id_new INT,
+	order_id_old INT,
+    employee_id_new INT,
+	employee_id_old INT,
+    action_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE OR REPLACE FUNCTION ft_audit_update_orders()
+RETURNS TRIGGER as $$
+BEGIN
+	insert into orders_audit_update(order_id_new,order_id_old,
+	employee_id_new,employee_id_old)
+	values(new.order_id,old.order_id,
+	new.employee_id,old.employee_id);
+	return new;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE TRIGGER tg_after_update_order
+AFTER UPDATE on orders
+FOR EACH ROW
+EXECUTE FUNCTION ft_audit_update_orders();
+
+select * from orders
+where order_id=11078
+
+select * from orders_audit_update
